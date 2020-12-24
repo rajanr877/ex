d3d60f327c611e97ca3cd6c9c1b7d7ea0bc348ce
wolf <rajanr877@gmail.com>
2020-12-24 09:51:04 +0530

    feature module added new oprations

--- feature_module.py
+++ feature_module.py
@@ -2,6 +2,7 @@
 
 def feature():
 	print('new feature is working here. Behind your eyes!')
+	print('This new feature has some more Operations.')
 
 if __name__  == "__main__":
-	feature()		
+	feature()
